b33fb01c4a2dac77c23413c94741297935d7e16b
Chakrika_Anousha_ <101462904+chakrikaanousha@users.noreply.github.com>
2022-07-24 00:25:22 +0530

    Update hasBad.java
    adding solution

--- java/String1/hasBad.java
+++ java/String1/hasBad.java
@@ -12,3 +12,24 @@ hasBad("xbadxx") → true
 hasBad("xxbadxx") → false
 */
 
+public boolean hasBad(String str) {
+  if(str.length()<3){  //less than 3: not possible
+    return false;
+  }
+  if(str.length()==3){ //equal to 3 : a chance possible: checking condition
+    return (str.substring(0,3)).equals("bad");
+  }
+  
+  //return true if "bad" appears starting at index 0 or 1 in the string
+  if((str.substring(0,3)).equals("bad")){ //(i) index at 0
+    return true;
+  }
+  else if(str.substring(1,4).equals("bad")){// (ii) index at 1
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+
