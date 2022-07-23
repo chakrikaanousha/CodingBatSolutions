002cfaf6e88ba678cd068c786eef59eb7fe56cb2
Chakrika_Anousha_ <101462904+chakrikaanousha@users.noreply.github.com>
2022-07-24 00:07:31 +0530

    Create hasBad.java

---
+++ java/String1/hasBad.java
@@ -0,0 +1,14 @@
+//link to the question: https://codingbat.com/prob/p139075
+*****************************************************************************
+
+/*QUESTION:
+Given a string, return true if "bad" appears starting at index 0 or 1 in the string, 
+such as with "badxxx" or "xbadxx" but not "xxbadxx". The string may be any length, 
+including 0. Note: use .equals() to compare 2 strings.
+
+CASES:
+hasBad("badxx") → true
+hasBad("xbadxx") → true
+hasBad("xxbadxx") → false
+*/
+
