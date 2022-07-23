70784bd226c5ce8c2231051d99d6a6bcce39762b
Chakrika_Anousha_ <101462904+chakrikaanousha@users.noreply.github.com>
2022-07-23 22:26:09 +0530

    added que+cases+sol

--- java/String1/nTwice.java
+++ java/String1/nTwice.java
@@ -1,1 +1,21 @@
+//Link to the Question: https://codingbat.com/prob/p174148
 
+/*QUESTION:
+Given a string and an int n, return a string made of the first and last n chars from the string. The string length will be at least n.
+
+CASES:
+nTwice("Hello", 2) → "Helo"
+nTwice("Chocolate", 3) → "Choate"
+nTwice("Chocolate", 1) → "Ce"
+*/
+
+public String nTwice(String str, int n) {
+  
+  //ex. Hello
+  int l = str.length();//5
+  String fh = str.substring(0,n);//0,2: He
+  String lh = str.substring(l-n,l);//3,4: lo
+  
+  return fh+lh;//Helo
+  
+}
