1f63afa86e660385f8d25af9046e0ed563aae633
Chakrika_Anousha_ <101462904+chakrikaanousha@users.noreply.github.com>
2022-07-24 18:16:42 +0530

    Update without2.java

--- java/String1/without2.java
+++ java/String1/without2.java
@@ -9,3 +9,21 @@ without2("HelloHi") → "HelloHi"
 without2("Hi") → ""
 */
 
+public String without2(String str) {
+ 
+ if (str.length()<1){ //without2("") → ""
+   return "";
+ } 
+ 
+ else if(str.length()==1){ //without2("x") → "x"
+   return str;
+ }
+  
+ else if(str.substring(0,2).equals(str.substring(str.length()-2,str.length()))){ //without2("HelloHe") → "lloHe"
+   return str.substring(2,str.length());
+ } 
+ 
+ else{
+   return str;
+ }
+}
