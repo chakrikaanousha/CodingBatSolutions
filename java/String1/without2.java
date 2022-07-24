db396b7892be7bb10f091939e06c0d8f99d4cd46
Chakrika_Anousha_ <101462904+chakrikaanousha@users.noreply.github.com>
2022-07-24 17:59:21 +0530

    Create without2.java

---
+++ java/String1/without2.java
@@ -0,0 +1,11 @@
+//link to the que: https://codingbat.com/prob/p142247
+
+/*Question:
+Given a string, if a length 2 substring appears at both its beginning and end, return a string without the substring at the beginning, so "HelloHe" yields "lloHe". The substring may overlap with itself, so "Hi" yields "". Otherwise, return the original string unchanged.
+
+Cases
+without2("HelloHe") → "lloHe"
+without2("HelloHi") → "HelloHi"
+without2("Hi") → ""
+*/
+
