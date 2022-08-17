050036f8826db16c6d825170ff92ab0ba009181e
Chakrika_Anousha_ <101462904+chakrikaanousha@users.noreply.github.com>
2022-08-17 22:24:48 +0530

    Update endOther.java
    added code

--- java/String2/endOther.java
+++ java/String2/endOther.java
@@ -8,3 +8,27 @@ endOther("Hiabc", "abc") → true
 endOther("AbC", "HiaBc") → true
 endOther("abc", "abXabc") → true*/
 
+
+public boolean endOther(String a, String b) {
+  int alen = a.length(); // length of a and b str.
+    int blen = b.length();
+
+//2cases arises here as we got to compare the last and start elements
+
+    if(alen>blen){ 
+        String frt = a.substring(alen-blen,alen); //alen>blen
+        String lst = b.substring(0,blen);
+        String lst1 = lst.toLowerCase();
+        String frt1 = frt.toLowerCase();
+        return(lst1.equals(frt1));
+    }else{//alen<blen
+        String frt = a.substring(0,alen); //blen>alen
+        String lst = b.substring(blen-alen,blen); 
+        String lst1 = lst.toLowerCase();
+        String frt1 = frt.toLowerCase();
+        return(lst1.equals(frt1));
+    }
+
+}
+
+
