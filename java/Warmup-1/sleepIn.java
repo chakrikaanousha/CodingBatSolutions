90ce9ba1734337e4299b0ad6281b17479dd93e08
Chakrika_Anousha_ <101462904+chakrikaanousha@users.noreply.github.com>
2022-08-23 22:23:17 +0530

    Create sleepIn.java

---
+++ java/Warmup-1/sleepIn.java
@@ -0,0 +1,10 @@
+//LINK TO THE QUESTION: https://codingbat.com/prob/p187868
+/*
+QUESTION:
+The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. 
+We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+
+CASES:
+sleepIn(false, false) → true
+sleepIn(true, false) → false
+sleepIn(false, true) → true*/
