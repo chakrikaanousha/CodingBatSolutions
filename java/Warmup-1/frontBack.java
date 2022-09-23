87606502245bc45c7db19ecba3a7a2f35adf42b1
Chakrika_Anousha_ <101462904+chakrikaanousha@users.noreply.github.com>
2022-09-23 11:41:52 +0530

    Create frontBack.java

---
+++ java/Warmup-1/frontBack.java
@@ -0,0 +1,10 @@
+//link: https://codingbat.com/prob/p123384
+
+/*
+Given a string, return a new string where the first and last chars have been exchanged.
+
+
+frontBack("code") → "eodc"
+frontBack("a") → "a"
+frontBack("ab") → "ba"
+*/
